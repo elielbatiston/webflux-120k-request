ee6d9379a830fcf59012f6b6112bf8fb6a6150c8
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-12-13 13:45:23 -0300

    created project

---
+++ src/main/java/com/fullcycle/webflux120krequest/Webflux120kRequestApplication.java
@@ -0,0 +1,13 @@
+package com.fullcycle.webflux120krequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webflux120kRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webflux120kRequestApplication.class, args);
+	}
+
+}
