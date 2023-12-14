0dcc2cafb433061f1922a70b1122701e9c957c90
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-12-13 21:13:07 -0300

    add test

--- src/test/java/com/fullcycle/webflux120krequest/Webflux120kRequestApplicationTests.java
+++ src/test/java/com/fullcycle/webflux120krequest/Webflux120kRequestApplicationTests.java
@@ -1,7 +1,14 @@
 package com.fullcycle.webflux120krequest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
 
 @SpringBootTest
 class Webflux120kRequestApplicationTests {
@@ -10,4 +17,25 @@ class Webflux120kRequestApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void test1() {
+
+		//mais recomendada
+		final Mono<String> stringMono = Mono.fromCallable(() -> "OI")
+			.subscribeOn(Schedulers.parallel());
+
+		StepVerifier.create(stringMono)
+			.expectNext("OI")
+			.expectComplete()
+			.verify();
+	}
+
+	@Test
+	public void test2() {
+		final String resultado = Mono.fromCallable(() -> "OI")
+			.subscribeOn(Schedulers.parallel())
+			.block(Duration.ofSeconds(2));
+
+		Assertions.assertEquals("OI", resultado);
+	}
 }
